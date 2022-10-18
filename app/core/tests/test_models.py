f25d7532e4ab88785d62baaf5dcd4f7ad68a2f24
etamar <etamar.levy@gmail.com>
2022-10-18 19:48:49 -0400

    implemented tests for tags with serializer and viewset

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -72,7 +72,7 @@ class ModelTests(TestCase):
 
         self.assertEqual(str(recipe), recipe.title)
 
-    def test_create_taf(self):
+    def test_create_tag(self):
         """Test creating a tag is successful."""
         user = create_user()
         tag = models.Tag.objects.create(user=user, name='Tag1')
